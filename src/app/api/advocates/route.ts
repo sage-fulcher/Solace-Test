9dfd0a5f770dd2d514f7ed8b119fb498ed0ab6bf
Sage Fulcher <42389235+sage-fulcher@users.noreply.github.com>
2025-08-04 16:00:02 -0400

    connect real DB

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -3,10 +3,11 @@ import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
+  console.log("yooooo");
   // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
-
-  return Response.json({ data });
+  if (db) {
+    const data = await db.select().from(advocates);
+    console.log("data", data);
+    return Response.json({ data });
+  } else return Response.json({ advocateData });
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,19 +2,13 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const setup = () => {
-  if (!process.env.DATABASE_URL) {
+  if (process.env.DATABASE_URL) {
     console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
+    const queryClient = postgres(process.env.DATABASE_URL);
+    const db = drizzle(queryClient);
+    return db;
   }
-
   // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
 };
 
 export default setup();
