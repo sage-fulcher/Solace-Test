3d466794a543cf4a460c7b4521b5bb1727603306
Sage Fulcher <42389235+sage-fulcher@users.noreply.github.com>
2025-08-04 18:35:16 -0400

    change how DB is returned

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,29 @@
+import { and, gt, ilike, or } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  console.log("yooooo");
-  // Uncomment this line to use a database
+export async function GET(cursor?: number, pageSize = 2, term?: string) {
   if (db) {
-    const data = await db.select().from(advocates);
+    const data = await db
+      .select()
+      .from(advocates)
+      .where(
+        and(
+          cursor ? gt(advocates.id, cursor) : undefined,
+          term
+            ? or(
+                ilike(advocates.firstName, term),
+                ilike(advocates.lastName, term),
+                ilike(advocates.degree, term),
+                // ilike(advocates.specialties.ge, term),
+                ilike(advocates.yearsOfExperience, term),
+                ilike(advocates.phoneNumber, term)
+              )
+            : undefined
+        )
+      )
+      .limit(pageSize);
     console.log("data", data);
     return Response.json({ data });
   } else return Response.json({ advocateData });
